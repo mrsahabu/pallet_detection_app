d0deb1510a8745eaa6273bcd0cb5739d83845946
sahabu <st747809@gmail.com>
2024-05-18 03:07:23 +0500

    added logout api

--- auth/routes.py
+++ auth/routes.py
@@ -1,9 +1,11 @@
+from typing import Dict
+
 from fastapi import APIRouter, status, Depends, Header
 from fastapi.security import OAuth2PasswordRequestForm
 # from fastapi_jwt_auth import AuthJWT
 from sqlalchemy.orm import Session
 from core.database import get_db
-from auth.services import get_token, get_refresh_token
+from auth.services import get_token, get_refresh_token, oauth2_scheme
 from fastapi import APIRouter, Request, Response, status, Depends, HTTPException
 
 
@@ -12,6 +14,8 @@ router = APIRouter(
     tags=["Auth"],
     responses={404: {"description": "Not found"}},
 )
+
+token_store: Dict[str, str] = {}
 
 
 @router.post("/token", status_code=status.HTTP_200_OK)
@@ -22,3 +26,9 @@ async def authenticate_user(data: OAuth2PasswordRequestForm = Depends(), db: Ses
 @router.post("/refresh", status_code=status.HTTP_200_OK)
 async def refresh_access_token(refresh_token: str = Header(), db: Session = Depends(get_db)):
     return await get_refresh_token(token=refresh_token, db=db)
+
+@router.post("/logout")
+async def logout(token: str = Depends(oauth2_scheme)):
+    if token in token_store:
+        del token_store[token]
+    return {"message": "Successfully logged out"}
